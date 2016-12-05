2f20e67b2c35b7d8bb94cbfe20405d2bf0b15748
RLOG89 <ross.loggie@gmail.com>
2016-12-05 14:29:02 +0000

    part 1 refactor

--- day_3/triangles.js
+++ day_3/triangles.js
@@ -9,9 +9,6 @@ var triData = triLines.map(function(line) {
 
   var results = []
   for (var i = 0; i < triData.length; i++) {
-    var triData[i][0] = a
-    var triData[i][2] = b
-    var triData[i][3] = c
     if ((triData[i][0] + triData[i][1] > triData[i][2]) && 
           (triData[i][1] + triData[i][2] > triData[i][0]) &&
             (triData[i][0] + triData[i][2] > triData[i][1]))
