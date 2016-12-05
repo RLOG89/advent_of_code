b9c7117d6160f71f7f6fcfdcfd7028f5fba89472
RLOG89 <ross.loggie@gmail.com>
2016-12-05 00:27:54 +0000

    part 2 complete

--- day_2/specs/keypad.js
+++ day_2/specs/keypad.js
@@ -28,11 +28,36 @@ describe('Keypad', function() {
           '9': { U: '6', D: '9', L: '8', R: '9' },
       };
 
+/*
+          1
+        2 3 4
+      5 6 7 8 9
+        A B C
+          D
+*/
+      positions2 = 
+      { 
+        '1': { U: '1', D: '3', L: '1', R: '1'},
+        '2': { U: '2', D: '6', L: '2', R: '3'},
+        '3': { U: '1', D: '7', L: '2', R: '4'},
+        '4': { U: '4', D: '8', L: '3', R: '4'},
+        '5': { U: '5', D: '5', L: '5', R: '6'},
+        '6': { U: '2', D: 'A', L: '5', R: '7'},
+        '7': { U: '3', D: 'B', L: '6', R: '8'},
+        '8': { U: '4', D: 'C', L: '7', R: '9'},
+        '9': { U: '9', D: '9', L: '8', R: '9'},
+        'A': { U: '6', D: 'A', L: 'A', R: 'B'},
+        'B': { U: '7', D: 'D', L: 'A', R: 'C'},
+        'C': { U: '8', D: 'C', L: 'B', R: 'C'},
+        'D': { U: 'B', D: 'D', L: 'D', R: 'D'},
+      };
+
     keypad = new Keypad(instructions)
   });
 
   it('should give 5 digit code', function() {
-    assert.equal(00000, keypad.getPosition('5', positions));
+    // assert.equal(00000, keypad.getPosition('5', positions));
+    assert.equal(000000000, keypad.getPosition('5', positions2))
   });
 
 });
